9a025f397ae7c253109df6f920d91daa1451d6d9
Pieter Polmans <pieter.polmans@protonmail.com>
2024-12-05 13:03:07 +0100

    fix day4.2 solution
    Signed-off-by: Pieter Polmans <pieter.polmans@protonmail.com>

--- Day4/2.py
+++ Day4/2.py
@@ -12,35 +12,27 @@ def rotate(matrix):
     return [list(reversed(row)) for row in transposed]
 
 
-def sub_matrix(matrix, x, y):
-    row_1 = matrix[y][x:x+3]
-    row_2 = matrix[y+1][x:x+3]
-    row_3 = matrix[y+2][x:x+3]
-    return [row_1, row_2, row_3]
-
-
-def is_x_mas(matrix):
-    if not    matrix[0][0] == 'M' \
-       or not matrix[0][2] == 'S' \
-       or not matrix[1][1] == 'A' \
-       or not matrix[2][0] == 'M' \
-       or not matrix[2][2] == 'S':
-        return False
-    return True
-
-
 def count_x_masses(matrix):
     width, height = dimensions(matrix)
     xmas_count = 0
-    for x in range(width - 3):
-        for y in range(height - 3):
-            if is_x_mas(sub_matrix(matrix, x, y)):
+    for x in range(width - 2):
+        for y in range(height - 2):
+            if matrix[y][x] == 'M' \
+               and matrix[y+2][x] == 'M' \
+               and matrix[y][x+2] == 'S' \
+               and matrix[y+2][x+2] == 'S' \
+               and matrix[y+1][x+1] == 'A':
                 xmas_count += 1
     return xmas_count
 
 
 def dimensions(matrix):
     return (len(matrix[0]), len(matrix))
+
+
+def display(matrix):
+    for row in matrix:
+        print(''.join(row))
 
 
 def main():
